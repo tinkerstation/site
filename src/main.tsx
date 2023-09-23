c2955e2ffcc3fc69decc1b3ffb99c74bcd617362
Daniyal Ibrahim <48337074+daniyalIbrahim@users.noreply.github.com>
2023-09-23 02:28:41 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <LanguageContextProvider>
     <ConsentContextProvider>
       <CustomThemeProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/xenorbit" >
           <AppRouter />
         </BrowserRouter>
       </CustomThemeProvider>
